6c2a579de60179137dc831d91d3758628e31cf9b
mhan2000 <betitoas3@hotmail.com>
2022-10-29 19:46:08 -0500

    Se agregan comentarios de multiples lineas, ademas de que ahora se lee el codigo fuente por un archivo de texto llamado archivo.txt, tambien se agregan mas casos de prueba

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Principal {
+	
+	static String codigoFuente = "";
+	
+	public static void abrirArchivo(String ruta) {
+		BufferedReader lector;
+		try {
+			// Inicializar nuestro objeto encargado de leer nuestro archivo, asi como asignarle nuestro archivo a leer
+			lector = new BufferedReader(new FileReader(ruta));
+			// Leer primera linea
+			String linea = lector.readLine();
+			// Leer todas las linea mientras linea y agregarla a codigocodigoFuente mientras sea diferente de null
+			while(linea != null) {
+				// Agregar linea actual a nuestra variable codigocodigoFuente
+				codigoFuente += linea;
+				// Leer la siguiente linea
+				linea = lector.readLine();
+			}
+		} catch (IOException e) {
+			// En caso de que algo falle imprimir el error
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		// Codigo fuente
-		String fuente = "//Esto es un comentario del anio 2022 de 1 linea// if (((41e233 + 323.134442) << 10.54)) begin: mario = fact((4)) almaguer = pow((4,2)) end/* //Programa de lexico mostrando gran parte de la gramatica asignada// ";
+		// Leer archivo
+		abrirArchivo("src/archivo.txt");
 		// Limpiar espacios
-		fuente = fuente.replaceAll("\\s", "");
-		// Remover comentarios
-		fuente = fuente.replaceAll("[\\/][\\/][a-zA-z0-9]*[\\/][\\/]", "");
-		//Imprimir codigo fuente
-		System.out.println(fuente);
+		codigoFuente = codigoFuente.replaceAll("\\s", "");
+		// Remover comentarios simples y multiples
+		codigoFuente = codigoFuente.replaceAll("([\\/][\\/][a-zA-z0-9,\"]*[\\/][\\/])|([\\/][*][a-zA-z0-9,\"]*[*])", "");		//Imprimir codigo codigoFuente
+		System.out.println(codigoFuente);
 		// Instanciar clase lexico
 		Lexico lexico = new Lexico();
-		// Analizar codigo fuente
-		lexico.analizar(fuente);
+		// Analizar codigo codigoFuente
+		lexico.analizar(codigoFuente);
 		// Imprimir lexemas
 		System.out.println(lexico.lexemas);
 	}
